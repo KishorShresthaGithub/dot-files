f7f51fae7b021aee04c684a15cdbf2012cc3cbdf
KishorShresthaGithub <shrestha.kishor@grrow.jp>
2024-10-17 15:41:25 +0900

    Fix plugins

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -25,7 +25,7 @@ return require('packer').startup(function(use)
        requires  = {{'nvim-lua/plenary.nvim'}}
      }
 
-     use  {
+     use {
       'williamboman/mason.nvim' ,
       'williamboman/mason-lspconfig.nvim',
       'neovim/nvim-lspconfig',
